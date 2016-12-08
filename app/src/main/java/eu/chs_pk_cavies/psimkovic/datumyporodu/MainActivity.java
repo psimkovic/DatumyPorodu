cb5e382d4c0ea7501ad9544d45c21734aa221107
psimkovic <psimkovic@gmail.com>
2016-12-08 13:09:54 +0100

    vytvorenie

---
+++ app/src/main/java/eu/chs_pk_cavies/psimkovic/datumyporodu/MainActivity.java
@@ -0,0 +1,94 @@
+package eu.chs_pk_cavies.psimkovic.datumyporodu;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class MainActivity extends Activity {
+
+    private SimpleDateFormat dateFormatter;
+    private Calendar newDate = Calendar.getInstance();
+    private DatePicker datePicker;
+    private EditText editText2;
+    private EditText editText3;
+    private EditText editText4;
+    private TextView txtVerzia;
+    public String versionName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dateFormatter = new SimpleDateFormat("dd.MM.yyyy", Locale.GERMAN);
+
+        findViewsById();
+
+        datePicker.init(
+                newDate.get(Calendar.YEAR),
+                newDate.get(Calendar.MONTH),
+                newDate.get(Calendar.DAY_OF_MONTH),
+                new DatePicker.OnDateChangedListener() {
+                    @Override
+                    public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        setDateTimeField();
+                    }
+                });
+        try {
+            versionName = getApplicationContext().getPackageManager().getPackageInfo(getApplicationContext().getPackageName(), 0).versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        txtVerzia.setText(getString(R.string.lblAuthor)+", "+ getString(R.string.lblAppVersion) + versionName);
+    }
+
+    private void findViewsById() {
+        datePicker = (DatePicker) findViewById(R.id.datePicker);
+
+        editText2 = (EditText) findViewById(R.id.editText2);
+        editText2.setInputType(InputType.TYPE_NULL);
+
+        editText3 = (EditText) findViewById(R.id.editText3);
+        editText3.setInputType(InputType.TYPE_NULL);
+
+        editText4 = (EditText) findViewById(R.id.editText4);
+        editText4.setInputType(InputType.TYPE_NULL);
+
+        txtVerzia = (TextView) findViewById(R.id.txtVerzia);
+        txtVerzia.setInputType(InputType.TYPE_NULL);
+    }
+
+    private void setDateTimeField() {
+        newDate.set(datePicker.getYear(), datePicker.getMonth(), datePicker.getDayOfMonth());
+
+        Calendar c2 = Calendar.getInstance();
+        c2.setTime(newDate.getTime());
+        c2.add(Calendar.DATE, 59);
+        String newDateStr2 = dateFormatter.format(c2.getTime());
+        editText2.setText(newDateStr2);
+
+        Calendar c3 = Calendar.getInstance();
+        c3.setTime(newDate.getTime());
+        c3.add(Calendar.DATE, 68);
+        String newDateStr3 = dateFormatter.format(c3.getTime());
+        editText3.setText(newDateStr3);
+
+        Calendar c4 = Calendar.getInstance();
+        c4.setTime(newDate.getTime());
+        c4.add(Calendar.DATE, 74);
+        String newDateStr4 = dateFormatter.format(c4.getTime());
+        editText4.setText(newDateStr4);
+
+    }
+
+}
